76294857c6b1103e194227fdebdb6aabf07aaa11
FerBendala <bendalier@gmail.com>
2021-06-24 01:28:36 +0200

    dynamic labels

--- static/js/components/generic.js
+++ static/js/components/generic.js
@@ -1,4 +1,13 @@
 // trigger when page is ready
 $(document).ready(function () {
 
+    // Show label on value
+    $('select, input').on('input click', function () {
+        $(this).siblings('label').addClass('active');
+    });
+    $('select, input').on('focusout', function () {
+        if (!$(this).val()) {
+            $(this).siblings('label').removeClass('active');
+        }
+    });
 });
